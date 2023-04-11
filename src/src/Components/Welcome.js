31e312c1115a56aed5a331b37228ecd4d57931cd
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-04-12 00:13:42 +0700

    change app name

--- src/src/Components/Welcome.js
+++ src/src/Components/Welcome.js
@@ -1,9 +1,9 @@
 const Welcome = () => {
     return ( 
         <div className='welcome'>
-            <h3>Welcome to Miku Path!</h3>
+            <h3>Welcome to Akasha Maps!</h3>
             <label>Click app button to use our program</label>
-            <label>Miku Path is a web-based application to {"\n"} calculate shortest path for 2 points in Google Map</label>
+            <label>Akasha Maps is a web-based application to {"\n"} calculate shortest path for 2 points in Google Map</label>
         </div>
      );
 }
